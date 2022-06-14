d65bd9b60137c15ea6afbc056c4291546520debb
Łukasz Nowogórski <nowogorski.lukasz0@gmail.com>
2022-06-14 12:06:24 +0200

    1.Add class Age 2.Add class Name to show name 3.Add class SumSubstractMultiplyTwoNumbers to read numbers and sum, substract and multiply

---
+++ src/Name.java
@@ -0,0 +1,13 @@
+
+//Napisz metodę, która zwróci Twoje imię,
+class Name {
+    private static final String name = "Lukasz";
+
+    public static void getName(){
+        System.out.println("Your name: " + name);
+    }
+
+    public static void main(String[] args) {
+        getName();
+    }
+}
