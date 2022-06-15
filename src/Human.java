f570aea7635c50b13ee1715f78c4177cfe8b062a
Łukasz Nowogórski <luxus0@gmail.com>
2022-06-15 12:24:15 +0200

    Simple change

--- src/Human.java
+++ src/Human.java
@@ -1,6 +1,6 @@
 /*
-    Utwórz klasę Human reprezentującą człowieka, musi posiadać atrybuty takie jak wiek, waga, wzrost, imię i płeć.
-    Klasa powinna także zawierać metody getAge, getWeight, getHeight, getName, isMale
+   Utwórz klasę Human reprezentującą człowieka, musi posiadać atrybuty takie jak wiek, waga, wzrost, imię i płeć.
+   Klasa powinna także zawierać metody getAge, getWeight, getHeight, getName, isMale
 */
 
 class Human {
