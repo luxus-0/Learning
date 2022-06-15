9a2382d78abfceb3a2b211ad347549f63e3d84dd
Łukasz Nowogórski <luxus0@gmail.com>
2022-06-15 12:26:48 +0200

    Change

--- src/Age.java
+++
@@ -1,7 +0,0 @@
-//Napisz metodę, która zwróci Twój aktualny wiek.
-public class Age {
-    public static void main(String[] args) {
-        int age = 36;
-        System.out.println("Your age is: " +age);
-    }
-}
