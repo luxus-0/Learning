74aaa81033ba9056c12d266f1b64da4f9a6284f4
Łukasz Nowogórski <luxus0@gmail.com>
2022-06-17 12:09:39 +0200

    Add class First AlphabetLetter 1.Return array String with first 5 letter 2.Return revert array String with get array with 3 elements.

---
+++ src/Samouczek_programisty/Array/AlphabetLetter.java
@@ -0,0 +1,27 @@
+package Samouczek_programisty.Array;
+/*
+    1.Napisz metodę, która zwróci tablicę String[] zawierającą pierwsze 5 liter alfabetu
+    2.Napisz metodę pobierającą trójelementową tablicę liczb, która zwróci tablicę zawierającą te same elementy w odwróconej kolejności
+*/
+
+import java.util.Arrays;
+
+public class AlphabetLetter {
+
+    public static void main(String[] args) {
+        String[] letterAlphabet = getFiveAlphabetLetter();
+        System.out.println(Arrays.toString(letterAlphabet));
+
+        double[] revertArray = revert(new double[]{1, 2, 3});
+        System.out.println(Arrays.toString(revertArray));
+    }
+
+    private static String[] getFiveAlphabetLetter() {
+        return new String[] {"a", "ą", "b", "c", "ć"};
+    }
+
+    private static double[] revert(double[] array) {
+        return new double[] {array[2], array[1], array[0]};
+    }
+}
+
