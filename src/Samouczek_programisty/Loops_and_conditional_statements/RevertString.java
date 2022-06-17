78cedd63d92ab81d8c854e7696dd2edb0dac1c82
Łukasz Nowogórski <luxus0@gmail.com>
2022-06-17 14:10:22 +0200

    Add methods:  getRevertString(String) to revert String to separate character
     getRevertStringVersion2 - use StringBuilder - fast used to revert String

---
+++ src/Samouczek_programisty/Loops_and_conditional_statements/RevertString.java
@@ -0,0 +1,28 @@
+package Samouczek_programisty.Loops_and_conditional_statements;
+
+/*
+Utwórz metodę, która jako argument pobierze obiekt klasy String i zwróci “odwrócony” argument.
+Na przykład “pies” przekształci w “seip”,
+* */
+
+public class RevertString {
+
+    public static void getRevertString(String str) {
+        char[] characterArray = str.toCharArray();
+
+        for(int index = characterArray.length - 1; index >= 0; index--) {
+            System.out.print(characterArray[index]);
+        }
+    }
+
+    public static String getRevertStringVersion2(String str) {
+        StringBuilder stringBuilder = new StringBuilder(str).reverse();
+        return stringBuilder.toString();
+    }
+
+    public static void main(String[] args) {
+        getRevertString("pies");
+        String revertString = getRevertStringVersion2("piesek");
+        System.out.println("\n"+revertString);
+    }
+}
