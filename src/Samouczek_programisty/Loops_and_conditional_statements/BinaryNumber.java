5c5c1a110d1e01024c315c4e3ab4d27b52b1cb90
Łukasz Nowogórski <luxus0@gmail.com>
2022-06-19 00:15:02 +0200

    Add version 3 convert from decimal to binary number use Math.pow without array

--- src/Samouczek_programisty/Loops_and_conditional_statements/BinaryNumber.java
+++ src/Samouczek_programisty/Loops_and_conditional_statements/BinaryNumber.java
@@ -17,16 +17,31 @@ class BinaryNumber {
 
         int[] binaryArray = new int[1000];
         int i = 0;
-        while(number > 0) {
+        while (number > 0) {
             binaryArray[i] = number % 2;
             number = number / 2;
             i++;
         }
 
         //print binary array in reverse order
-        for(int j = i - 1; j >= 0; j--) {
+        for (int j = i - 1; j >= 0; j--) {
             System.out.print(binaryArray[j]);
         }
+    }
+
+    //used Math.pow without Array
+    public static int getDecToBinary(int number) {
+
+        int binaryNumber = 0;
+        int power = 0;
+        while (number != 0) {
+            int mod = number % 2;
+            double c = Math.pow(10, power);
+            binaryNumber += mod * c;
+            number /= 2;
+            power++;
+        }
+        return binaryNumber;
     }
 
     public static void main(String[] args) {
@@ -37,6 +52,10 @@ class BinaryNumber {
 
         //version 2
         getDecimalToBinary(23);
+
+        //version 3
+        int decimalToBinary = getDecToBinary(23);
+        System.out.println(decimalToBinary);
     }
 
 }
