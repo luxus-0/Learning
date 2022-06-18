f96b40160d5bc74e4b319566bfae32e2a1092c83
Łukasz Nowogórski <luxus0@gmail.com>
2022-06-19 00:07:28 +0200

    Add version 2 convert from decimal to binary number use array

--- src/Samouczek_programisty/Loops_and_conditional_statements/BinaryNumber.java
+++ src/Samouczek_programisty/Loops_and_conditional_statements/BinaryNumber.java
@@ -7,13 +7,36 @@ package Samouczek_programisty.Loops_and_conditional_statements;
 
 class BinaryNumber {
 
+    //short version
     public static String convertFromDecimalToBinary(int number) {
         return Integer.toBinaryString(number);
     }
 
+    //array version
+    public static void getDecimalToBinary(int number) {
+
+        int[] binaryArray = new int[1000];
+        int i = 0;
+        while(number > 0) {
+            binaryArray[i] = number % 2;
+            number = number / 2;
+            i++;
+        }
+
+        //print binary array in reverse order
+        for(int j = i - 1; j >= 0; j--) {
+            System.out.print(binaryArray[j]);
+        }
+    }
+
     public static void main(String[] args) {
+
+        //version 1
         String convertToBinary = convertFromDecimalToBinary(23);
         System.out.println(convertToBinary);
+
+        //version 2
+        getDecimalToBinary(23);
     }
 
 }
