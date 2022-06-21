33415430ca755f5ac84f481ba9241bcea5409efe
Łukasz Nowogórski <luxus0@gmail.com>
2022-06-21 21:06:18 +0200

    Add class Human with Objects Type

---
+++ src/Samouczek_programisty/primitivetypes/Human.java
@@ -0,0 +1,49 @@
+package Samouczek_programisty.primitivetypes;
+
+class Human {
+    private String name;
+    private Integer age;
+    private Float wage;
+    private Double height;
+    private Boolean isMale;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Float getWage() {
+        return wage;
+    }
+
+    public void setWage(Float wage) {
+        this.wage = wage;
+    }
+
+    public Double getHeight() {
+        return height;
+    }
+
+    public void setHeight(Double height) {
+        this.height = height;
+    }
+
+    public Boolean getMale() {
+        return isMale;
+    }
+
+    public void setMale(Boolean male) {
+        isMale = male;
+    }
+}
