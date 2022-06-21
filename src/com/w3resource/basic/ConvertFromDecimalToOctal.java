400d36859f60ca6608d144977da99567692269bc
Łukasz Nowogórski <luxus0@gmail.com>
2022-06-21 23:25:18 +0200

    Add class with 2 methods: readDecimal(String) and printOctalNumbers()
    Read Decimal number
    and
    Convert from decimal to Octal

---
+++ src/com/w3resource/basic/ConvertFromDecimalToOctal.java
@@ -0,0 +1,37 @@
+package com.w3resource.basic;
+
+/*
+*Write a Java program to convert a decimal number
+* to octal number
+* Input Data:
+Input a decimal number: 15
+* */
+
+import java.util.Scanner;
+
+class ConvertFromDecimalToOctal {
+
+    public static int readDecimal(String message) {
+        System.out.println(message);
+        return new Scanner(System.in).nextInt();
+    }
+
+    public static void printOctalNumber() {
+        int i= 1,j;
+        int decimalNumber = readDecimal("Input  decimal number");
+        int[] octalArray = new int[40];
+        while(decimalNumber > 0) {
+            octalArray[i++] = decimalNumber % 8;
+            decimalNumber /= 8;
+        }
+        System.out.print("Octal number is: ");
+        for(j=i-1; j>0; j--) {
+            System.out.print(octalArray[j]);
+        }
+        System.out.print("\n");
+    }
+
+    public static void main(String[] args) {
+        printOctalNumber();
+    }
+}
