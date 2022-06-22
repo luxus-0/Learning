3693bf70e1c593ce7838dc33d56aedde19da6faf
Łukasz Nowogórski <luxus0@gmail.com>
2022-06-23 00:12:11 +0200

    Not finished class

---
+++ src/com/w3resource/basic/CountLettersSpaceNumbersCharacter.java
@@ -0,0 +1,15 @@
+package com.w3resource.basic;
+
+/*
+* Write a Java program to count the letters, spaces, numbers
+* and other characters of an input string
+* Expected Output
+The string is :  Aa kiu, I swd skieo 236587. GH kiu: sieo?? 25.33
+letter: 23
+space: 9
+number: 10
+other: 6
+* */
+
+public class CountLettersSpaceNumbersCharacter {
+}
