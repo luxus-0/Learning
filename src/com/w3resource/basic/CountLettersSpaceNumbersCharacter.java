8fd21db6b4f160891a678a8f26bd94542e82da25
Łukasz Nowogórski <luxus0@gmail.com>
2022-06-23 22:32:01 +0200

    Add 3 methods: countLetter(String) countDigit(String) countSpace(String)
    All method counter Letter Digit space in String

--- src/com/w3resource/basic/CountLettersSpaceNumbersCharacter.java
+++ src/com/w3resource/basic/CountLettersSpaceNumbersCharacter.java
@@ -8,8 +8,54 @@ The string is :  Aa kiu, I swd skieo 236587. GH kiu: sieo?? 25.33
 letter: 23
 space: 9
 number: 10
-other: 6
 * */
 
 public class CountLettersSpaceNumbersCharacter {
+
+    public static int countLetter(String letter) {
+        int counterLetter = 0;
+        char[] charArray = letter.toCharArray();
+
+        for (char c : charArray) {
+            if (Character.isLetter(c)) {
+                counterLetter++;
+            }
+        }
+        return counterLetter;
+    }
+
+    public static int countDigit(String digit) {
+        int counterDigit = 0;
+        char[] charArray = digit.toCharArray();
+
+        for (char digitCharacter : charArray) {
+            if (Character.isDigit(digitCharacter))
+                counterDigit++;
+        }
+        return counterDigit;
+    }
+
+    public static int countSpace(String space) {
+        int counterSpace = 0;
+        char[] charArray = space.toCharArray();
+
+        for (char digitCharacter : charArray) {
+            if (Character.isSpaceChar(digitCharacter))
+                counterSpace++;
+        }
+        return counterSpace;
+    }
+
+    public static void main(String[] args) {
+        String str = "Aa kiu, I swd skieo 236587. GH kiu: sieo?? 25.33";
+        int count = countLetter(str);
+        System.out.printf("Letters%s%n", " : " + count);
+
+        int count2 = countDigit(str);
+        System.out.printf("Digit%s%n", " : " + count2);
+
+        int count3 = countSpace(str);
+        System.out.printf("Space%s%n", " : " + count3);
+
+    }
 }
