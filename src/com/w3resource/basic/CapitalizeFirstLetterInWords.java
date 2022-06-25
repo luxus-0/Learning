824705414c2d732fecaad381b9923fac2ff86e1f
Łukasz Nowogórski <luxus0@gmail.com>
2022-06-25 23:28:36 +0200

    Method Capitalize First letter in Words change from small letter to big letter.

---
+++ src/com/w3resource/basic/CapitalizeFirstLetterInWords.java
@@ -0,0 +1,27 @@
+package com.w3resource.basic;
+
+/*
+* Write a Java program to capitalize the first letter of each word in a sentence.
+* */
+public class CapitalizeFirstLetterInWords {
+
+    private static String capitalizeFirstLetterInWords() {
+        StringBuilder capitalizeLetterBuilder = new StringBuilder();
+        String str = "what is the weather today?";
+        String[] words = str.split(" ");//or split("//s")
+        for(String word : words) {
+            String firstLetter = word.substring(0, 1);
+            String remainingLetter = word.substring(1);
+            capitalizeLetterBuilder
+                    .append(firstLetter.toUpperCase())
+                    .append(remainingLetter)
+                    .append(" ");
+        }
+        return capitalizeLetterBuilder.toString();
+    }
+    
+    public static void main(String[] args) {
+        String capitalizeLetter = capitalizeFirstLetterInWords();
+        System.out.println(capitalizeLetter);
+    }
+}
