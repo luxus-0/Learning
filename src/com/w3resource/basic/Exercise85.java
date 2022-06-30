6e7d67f817aa3ebc0224885acbf62a413b6422d4
Łukasz Nowogórski <luxus0@gmail.com>
2022-06-30 13:09:08 +0200

    Exercise

---
+++ src/com/w3resource/basic/Exercise85.java
@@ -0,0 +1,15 @@
+package com.w3resource.basic;
+
+/*
+* Write a Java program to check if a string starts with a specified word. Go to the editor
+Sample Data: string1 = "Hello how are you?"
+Sample Output:
+true
+* */
+
+public class Exercise85 {
+    public static void main(String[] args) {
+        String str = "Hello how are you";
+        System.out.println(str.startsWith("Hello"));
+    }
+}
