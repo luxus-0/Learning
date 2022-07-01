db1950563c98da3f8380b9b76ec55af7c1d71a14
Łukasz Nowogórski <luxus0@gmail.com>
2022-07-01 21:30:49 +0200

    Exercise112

---
+++ src/com/w3resource/basic/Exercise112.java
@@ -0,0 +1,44 @@
+package com.w3resource.basic;
+
+/*
+* Write a Java program to compute the number of trailing zeros in a factorial.
+7! = 5040, therefore the output should be 1
+* */
+
+public class Exercise112 {
+
+    //recursion
+    public static long calculateFactorial(int number) {
+        if(number <= 2) {
+            return number;
+        }
+        return number * calculateFactorial(number - 1);
+    }
+
+    //
+    public static long calculateFactorialVersion2(int number) {
+        long fact = 1;
+        for(int i = 2; i <= number; i++) {
+            fact *= i;
+        }
+        return fact;
+    }
+
+    public static long computeNumberOfTrailingZeroInFactorial(int number) {
+        long countTrailing = 0;
+        while (number != 0)
+        {
+            countTrailing += number / 5;
+            number /= 5;
+        }
+        return countTrailing;
+    }
+
+    public static void main(String[] args) {
+        int number = 14;
+        long factorial = calculateFactorial(number);
+        System.out.printf("factorial: %d" , factorial);
+        long countTrailingZero = computeNumberOfTrailingZeroInFactorial(number);
+        System.out.println("   " +countTrailingZero);
+    }
+}
