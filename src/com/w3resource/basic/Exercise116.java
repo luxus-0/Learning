9b7d2342d4ce8e8f1b3e6d8b4c724b1c0a27847e
Łukasz Nowogórski <luxus0@gmail.com>
2022-07-02 15:50:01 +0200

    Exercise116

---
+++ src/com/w3resource/basic/Exercise116.java
@@ -0,0 +1,22 @@
+package com.w3resource.basic;
+
+/*
+* Write a Java program which iterates the integers from 1 to 100. For multiples of three print "Fizz"
+* instead of the number and print "Buzz" for the multiples of five. When number is divided by both three and five,
+* print "fizz buzz".
+* */
+
+
+public class Exercise116 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            if (i % 5 == 0 && i % 3 == 0) {
+                System.out.println("Fizz Buzz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            }
+        }
+    }
+}
