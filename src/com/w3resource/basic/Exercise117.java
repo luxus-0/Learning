7730ab08857874ed37dc9ad009fdb5bae01f104b
Łukasz Nowogórski <luxus0@gmail.com>
2022-07-02 15:50:09 +0200

    Exercise117

---
+++ src/com/w3resource/basic/Exercise117.java
@@ -0,0 +1,11 @@
+package com.w3resource.basic;
+
+/*
+* Write a Java program to get the first occurrence (Position starts from 0.) of a string within a given string.
+ * */
+
+public class Exercise117 {
+    public static void main(String[] args) {
+
+    }
+}
