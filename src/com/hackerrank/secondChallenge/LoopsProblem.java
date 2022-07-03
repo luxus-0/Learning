36ea393676777fec3342a8232a7c112e7d6c5944
Łukasz Nowogórski <luxus0@gmail.com>
2022-07-04 01:13:59 +0200

    Loops problem - easy

---
+++ src/com/hackerrank/secondChallenge/LoopsProblem.java
@@ -0,0 +1,16 @@
+package com.hackerrank.secondChallenge;
+
+import java.util.Scanner;
+
+public class LoopsProblem {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        for (int i = 1; i <= 10; i++) {
+            if (N <= 20 && N >= 2) {
+                int result = N * i;
+                System.out.println(N + " x " + i + " = " + result);
+            }
+        }
+    }
+}
