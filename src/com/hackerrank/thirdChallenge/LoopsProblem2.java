479208ff754cb14106f8e9bd82a8ded9d16e90e9
Łukasz Nowogórski <luxus0@gmail.com>
2022-07-04 02:46:54 +0200

    Loops problem

---
+++ src/com/hackerrank/thirdChallenge/LoopsProblem2.java
@@ -0,0 +1,22 @@
+package com.hackerrank.thirdChallenge;
+
+import java.util.Scanner;
+
+public class LoopsProblem2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int q = sc.nextInt();
+
+        for (int i = 0; i < q; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int n = sc.nextInt();
+            int c = a;
+            for(int j = 0; j < n; j++) {
+                c += Math.pow(2, j) * b;
+                System.out.printf("%s ",c);
+            }
+            System.out.println();
+        }
+    }
+}
