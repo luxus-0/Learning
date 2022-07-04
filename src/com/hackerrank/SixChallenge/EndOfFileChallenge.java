cf0c93b6368fdd7d74bba66bb0491ef4885c7073
Łukasz Nowogórski <luxus0@gmail.com>
2022-07-05 00:14:06 +0200

    Six challenge - scanner.hasNext()

--- src/com/hackerrank/SixChallenge/EndOfFileChallenge.java
+++ src/com/hackerrank/SixChallenge/EndOfFileChallenge.java
@@ -4,10 +4,11 @@ import java.util.Scanner;
 
 public class EndOfFileChallenge {
     public static void main(String[] args) {
-        int i = 1;
-        Scanner sc = new Scanner(System.in);
-        while (sc.hasNext()) {
-            System.out.println(i++ + " " + sc.nextLine() + "\n" + i++ + " " + sc.nextLine() + "\n" + i++ + " " + sc.nextLine());
+        int i = 0;
+            Scanner sc = new Scanner(System.in);
+            while (sc.hasNext()) {
+                i++;
+                System.out.println(i + " " + sc.nextLine());
             }
-        }
     }
+}
