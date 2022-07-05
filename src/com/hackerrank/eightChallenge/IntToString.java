4248a470d48d3c7172eddbe5ea84b6fc2579581e
Łukasz Nowogórski <luxus0@gmail.com>
2022-07-05 20:35:00 +0200

    Covert from Int to String challenge

---
+++ src/com/hackerrank/eightChallenge/IntToString.java
@@ -0,0 +1,21 @@
+package com.hackerrank.eightChallenge;
+
+import java.util.Scanner;
+
+public class IntToString {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        String s = String.valueOf(n);
+
+        try {
+            if (!s.isEmpty() && n >= -100 && n <= 100) {
+                System.out.println("Good job");
+            } else {
+                throw new Exception("Give number in range <-100,100> !!!");
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
