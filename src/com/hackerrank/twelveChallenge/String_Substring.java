40f37f9aa3f053a1b9eba78cf6139bc498ba7f72
Łukasz Nowogórski <luxus0@gmail.com>
2022-07-06 22:37:26 +0200

    String substring challenge

---
+++ src/com/hackerrank/twelveChallenge/String_Substring.java
@@ -0,0 +1,25 @@
+package com.hackerrank.twelveChallenge;
+
+import java.util.Scanner;
+
+public class String_Substring {
+    private final static String REGEX = "^[a-zA-Z]*$";
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.next();
+
+        int start = scanner.nextInt();
+        int end = scanner.nextInt();
+
+        try {
+            if (s.matches(REGEX) && s.length() >= 1 && s.length() <= 100) {
+                System.out.println(s.substring(start, end));
+            } else {
+                throw new Exception("Not alphabetic letters");
+            }
+        } catch (Exception e) {
+            e.fillInStackTrace();
+        }
+    }
+}
