d9f2f6a9b86f46e9f45143411b34a7f0c323cbf8
Łukasz Nowogórski <luxus0@gmail.com>
2022-07-06 20:44:25 +0200

    String calculate sum length, lexicographically larger A than B, capitalize first letter in String A and B separate by space
    VERSION 2

---
+++ src/com/hackerrank/elevenChallenge/StringChallengeVersion2.java
@@ -0,0 +1,26 @@
+package com.hackerrank.elevenChallenge;
+
+import java.util.Scanner;
+
+public class StringChallengeVersion2 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String A = scanner.nextLine();
+        String B = scanner.nextLine();
+
+        System.out.println(A.length() + B.length());
+
+        //or
+        //System.out.println(A.compareTo(B) > 0 ? "Yes" : "No");
+        if (A.charAt(0) < B.charAt(0)) {
+            System.out.println("No");
+        } else {
+            System.out.println("Yes");
+        }
+
+        System.out.println(A.substring(0, 1).toUpperCase() + A.substring(1) + " "
+                + B.substring(0, 1).toUpperCase() + B.substring(1));
+    }
+}
+
