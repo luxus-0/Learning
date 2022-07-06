56f1ce6e551198bbc027bdc91550cb33e525ec8c
Łukasz Nowogórski <luxus0@gmail.com>
2022-07-06 20:43:55 +0200

    String calculate sum length, lexicographically larger A than B, capitalize first letter in String A and B separate by space
    VERSION 1

--- src/com/hackerrank/elevenChallenge/StringChallenge.java
+++ src/com/hackerrank/elevenChallenge/StringChallenge.java
@@ -3,19 +3,42 @@ package com.hackerrank.elevenChallenge;
 import java.util.Scanner;
 
 public class StringChallenge {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String A = scanner.nextLine();
-        String B = scanner.nextLine();
-
-        System.out.println(A.length() + B.length());
-
-                if (A.charAt(0) > B.charAt(0)) {
-                    System.out.println("Yes");
-                } else {
-                    System.out.println("No");
-                }
-
-                System.out.println(A.substring(0, 1).toUpperCase() + A.substring(1) + " " + B.substring(0, 1).toUpperCase() + B.substring(1));
-            }
+    public static String readString() {
+        return new Scanner(System.in).nextLine();
     }
+
+    public static void checkLexicographicallyLarger(String A, String B) {
+        if (A.charAt(0) > B.charAt(0)) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+
+        //or
+        //String lexicographically = A.compareTo(B) > 0 ? "Yes" : "No";
+        //System.out.println(lexicographically);
+
+    }
+
+    public static int sumLength(String A, String B) {
+        return A.length() + B.length();
+    }
+
+    public static String printCapitalizeFirstLetter(String A, String B) {
+        return A.substring(0, 1).toUpperCase() + A.substring(1) + " "
+                + B.substring(0, 1).toUpperCase() + B.substring(1);
+    }
+
+    public static void main(String[] args) {
+        String A = readString();
+        String B = readString();
+
+        int sumLengthString = sumLength(A, B);
+        System.out.println(sumLengthString);
+
+        checkLexicographicallyLarger(A, B);
+
+        String capitalizeFirstLetterInString =  printCapitalizeFirstLetter(A, B);
+        System.out.println(capitalizeFirstLetterInString);
+    }
+}
