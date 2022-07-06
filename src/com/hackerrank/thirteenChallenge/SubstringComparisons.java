5b3162e28f3c775d4a929b2f505de7df3382aa5e
Łukasz Nowogórski <luxus0@gmail.com>
2022-07-06 23:57:34 +0200

    Substring comparison used compareTo and print min and max.

---
+++ src/com/hackerrank/thirteenChallenge/SubstringComparisons.java
@@ -0,0 +1,35 @@
+package com.hackerrank.thirteenChallenge;
+
+import java.util.Scanner;
+
+public class SubstringComparisons {
+
+    public static String getSmallestAndLargest(String s, int k) {
+
+        String current = s.substring(0, k);
+        String min = current;
+        String max = current;
+
+        for (int i = k; i < s.length(); i++) {
+            if (Character.isAlphabetic(s.charAt(i)) && s.length() <= 1000) {
+                current = current.substring(1, k) + s.charAt(i);
+            }
+            if (max.compareTo(current) < 0) {
+                max = current;
+            }
+            if(min.compareTo(current) > 0){
+                min = current;
+            }
+        }
+        return min + "\n" + max;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.next();
+        int k = scanner.nextInt();
+
+        String smallestAndLargest = getSmallestAndLargest(s, k);
+        System.out.println(smallestAndLargest);
+    }
+}
