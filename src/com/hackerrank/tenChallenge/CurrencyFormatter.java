bf9a0d0f83c09ecd7f6cdb27d0da3c887055c0f2
Łukasz Nowogórski <luxus0@gmail.com>
2022-07-06 19:38:11 +0200

    Currency Formatter used NumberFormat

--- src/com/hackerrank/tenChallenge/CurrencyFormatter.java
+++ src/com/hackerrank/tenChallenge/CurrencyFormatter.java
@@ -8,11 +8,10 @@ public class CurrencyFormatter {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         double payment = scanner.nextDouble();
-
-        Locale indiaLocale = new Locale("en", "in");
+        scanner.close();
 
         NumberFormat u = NumberFormat.getCurrencyInstance(Locale.US);
-        NumberFormat i = NumberFormat.getCurrencyInstance(indiaLocale);
+        NumberFormat i = NumberFormat.getCurrencyInstance(new Locale("en", "In"));
         NumberFormat c = NumberFormat.getCurrencyInstance(Locale.CHINA);
         NumberFormat f = NumberFormat.getCurrencyInstance(Locale.FRANCE);
 
