1d8d09cda1cd1dd080aa804a89ed860ce5735d65
Łukasz Nowogórski <luxus0@gmail.com>
2022-07-11 00:55:44 +0200

    Regex words length from 8-30 lower case,upper case digit.First character start from big letter or small letter.

--- src/com/hackerrank/ValidUsernameChecker.java
+++ src/com/hackerrank/ValidUsernameChecker.java
@@ -9,6 +9,7 @@ import java.util.regex.Pattern;
 public class ValidUsernameChecker {
 
     private final static String REGEX = "^[a-zA-Z]\\w{7,29}$";
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = Integer.parseInt(scanner.nextLine());
