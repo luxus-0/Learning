dc44209dffbe1bff70024450104720a46a94b0ff
Łukasz Nowogórski <luxus0@gmail.com>
2022-07-12 14:09:40 +0200

    BigInteger with function add multiply

---
+++ src/com/hackerrank/BigIntegerAddMultiply.java
@@ -0,0 +1,16 @@
+package com.hackerrank;
+
+/*https://www.hackerrank.com/challenges/java-biginteger/problem?isFullScreen=true*/
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class BigIntegerAddMultiply {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        BigInteger a = scanner.nextBigInteger();
+        BigInteger b = scanner.nextBigInteger();
+        System.out.println(a.add(b));
+        System.out.println(a.multiply(b));
+    }
+}
